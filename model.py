636bbae08bbde71f6d05bc6bc3817564e91511e2
Hackbright Student <info@hackbrightacademy.com>
2016-02-02 13:10:01 -0800

    Added features for new user account creation and user sign in

--- model.py
+++ model.py
@@ -18,14 +18,14 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(64), nullable=True, unique=True)
     password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
     def __repr__(self):
         """Represents user object"""
-        return "<User ID: %s, Email: %s, Age: %d, Zipcode: %s>" % (self.user_id, self.email, self.age, self.zipcode)
+        return "<User ID: %s, Email: %s>" % (self.user_id, self.email)
 
 
 # Put your Movie and Rating model classes here.
@@ -54,8 +54,8 @@ class Rating(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     score = db.Column(db.Integer, nullable=False)
 
-    movie = db.relationship('Movie')
-    user = db.relationship('User')
+    movie = db.relationship('Movie', backref=db.backref('ratings', order_by=rating_id))
+    user = db.relationship('User', backref=db.backref('ratings', order_by=rating_id))
 
     def __repr__(self):
         """Represents user object"""
--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,67 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/signin')
+def sign_in():
+    """Allows users to login."""
+
+    return render_template("signin.html")
+
+@app.route('/confirm_signin', methods=['POST'])
+def confirm_sign_in():
+    """Confirms user login."""
+
+    username = request.form.get("user_email")
+    password = request.form.get("user_password")
+
+    user = User.query.filter_by(email=username).first()
+
+    if user:
+        if (user.password == password):
+            flash("Successfully logged in.")
+            return redirect('/')
+        else:
+            flash("Incorrect password. Please try again.")
+            return redirect('/signin')
+
+    else:
+        flash("Account does not exist. Please sign up!")
+        return redirect('/signup_form')
+
+
+@app.route('/signup_form')
+def sign_up():
+    """Allow new users to sign up."""
+
+    return render_template("signup_form.html")
+
+@app.route('/confirm_signup', methods=['POST'])
+def confirm_sign_up():
+    """Shows that user has successfully signed up."""
+
+    username = request.form.get("user_email")
+    password = request.form.get("user_password")
+
+    user_exists = User.query.filter_by(email=username).first()
+
+    if user_exists:
+        flash("User already exists. Please login.")
+    else:
+        user = User(email=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account was successfully created.")
+
+    return redirect('/signin')
 
 
 if __name__ == "__main__":
